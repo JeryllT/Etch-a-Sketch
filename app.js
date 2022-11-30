fcc533bb912ca4842de7e166e4b57483c695cec4
JT <jerylltan@gmail.com>
2022-11-30 13:58:11 +0800

    JS logic completed

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 const pixels = document.querySelector(".pixel-container");
-console.log(pixels)
+let currentShade = 0; 
 
-for(i=0; i < 16; i++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("row-div");
-    for(j = 0; j< 16; j++) {
-        let pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixel.addEventListener('mouseover', pix => pix.target.style.backgroundColor = "black");
-        rowDiv.appendChild(pixel);
-    }
-    pixels.appendChild(rowDiv);
+let blackWhiteFun = function(e) {
+    e.target.style.backgroundColor = "black";
 }
+
+let rgbFun = function (e) {
+    e.addEventListener("mouseover", pixListener => {
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    pixListener.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`
+    }, { once: true })
+}
+
+function start() {
+    for(i=0; i < 16; i++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("row-div");
+        for(j = 0; j< 16; j++) {
+            let pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            rowDiv.appendChild(pixel);
+        }
+        pixels.appendChild(rowDiv);
+        blackWhite();
+    }
+};
 
 function containerDim() {
     let newHeight = +prompt("What's the new height?", "100");
-    console.log(newHeight);
     while (isNaN(newHeight) || newHeight > 100 || newHeight < 1) {
         alert("Please ensure value is below 100 and is a valid number.");
         newHeight = +prompt("What's the new height?", "100");
     };
 
     let newWidth = +prompt("What's the new width?", "100");
-    console.log(newWidth);
     while (isNaN(newWidth) || newWidth > 100 || newWidth < 1) {
         alert("Please ensure value is below 100 and is a valid number.");
         newWidth = +prompt("What's the new height?", "100");
@@ -35,20 +48,49 @@ function containerDim() {
         for(j = 0; j< newWidth; j++) {
             let pixel = document.createElement("div");
             pixel.classList.add("pixel");
-            pixel.addEventListener('mouseover', pix => pix.target.style.backgroundColor = "black");
             rowDiv.appendChild(pixel);
         }
         pixels.appendChild(rowDiv);
     };
+    if (currentShade === 0) {
+        blackWhite()
+    } else {
+        randomRBG()
+    }
 };
 
-function randomRBG() {
+function blackWhite() {
+    currentShade = 0;
     const pixel = document.querySelectorAll(".pixel");
     pixel.forEach(pix => {
-        let red = Math.floor(Math.random() * 256);
-        let blue = Math.floor(Math.random() * 256);
-        let green = Math.floor(Math.random() * 256);
-        pix.addEventListener("mouseover", pixListener => pixListener.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`);
-    });
+        pix.addEventListener('mouseover', blackWhiteFun);
+    })
+}
+
+function randomRBG() {
+    currentShade = 1;
+    const pixel = document.querySelectorAll(".pixel");
+    pixel.forEach(pix => {
+        pix.removeEventListener("mouseover", blackWhiteFun);
+        rgbFun(pix);
+        })
 };
 
+function clearCanvas() {
+    const pixel = document.querySelectorAll(".pixel");
+
+    pixel.forEach(pix => {
+        pix.removeEventListener("mouseover", blackWhiteFun);
+        pix.removeEventListener("mouseover", rgbFun);
+        pix.style.backgroundColor = "aliceblue"
+    });
+
+    if (currentShade === 0) {
+        blackWhite()
+    } else {
+        randomRBG()
+    }
+
+};
+
+start();
